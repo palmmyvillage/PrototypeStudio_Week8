0e4decf2f306659e4c9f4e6cba6974477e4daf30
palmmyvillage <43654403+palmmyvillage@users.noreply.github.com>
2019-10-28 18:15:47 -0400

    ini ResetButton

---
+++ PrototypeStudio_Week8/Assets/Scripts/ResetButton.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetButton : MonoBehaviour
+{
+    public KeyCode resetScene, resetGame, quitGame, nextScene;
+
+    public bool allowToGoNextScene;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ResetScene();
+        ResetGame();
+        QuitGame();
+        
+        if (allowToGoNextScene == true)
+            goNextScene();
+    }
+
+    void ResetScene()
+    {
+        if (Input.GetKeyDown(resetScene))
+        {
+            Debug.Log("reserScene");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void ResetGame()
+    {
+        if (Input.GetKeyDown(resetGame))
+        {
+            Debug.Log("resetGame");
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    void QuitGame()
+    {
+        if (Input.GetKeyDown(quitGame))
+        {
+            Debug.Log("QuitGame");
+            Application.Quit();
+        }
+    }
+
+    void goNextScene()
+    {
+        if (Input.GetKeyDown(nextScene))
+        {
+            Debug.Log("toNextScene");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
